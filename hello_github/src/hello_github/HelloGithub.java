38ee49f38f127b2d509fd570e51f9e0da1599419
BenLi <21074986@qq.com>
2020-11-15 11:16:59 +0800

    Output 'Hello Github'.

---
+++ hello_github/src/hello_github/HelloGithub.java
@@ -0,0 +1,10 @@
+package hello_github;
+
+public class HelloGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github");
+	}
+
+}
