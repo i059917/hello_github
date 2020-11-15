d99eac1482b15e7d2a5c3cffbd4fb7ba678872db
BenLi <21074986@qq.com>
2020-11-15 11:20:28 +0800

    Add another line.

--- hello_github/src/hello_github/HelloGithub.java
+++ hello_github/src/hello_github/HelloGithub.java
@@ -5,6 +5,8 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
+		
+		System.out.println("Add another line");
 	}
 
 }
